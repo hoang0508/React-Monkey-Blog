714a418b4489aa1768d0b6ce3bc4b0bffddeacf8
hoang0508 <nguyenhuyhoang05082001@gmail.com>
2023-04-17 15:22:54 +0700

    h21

--- .history/src/module/post/PostAddNew_20220520224538.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import styled from "styled-components";
-
-const PostAddNewStyles = styled.div``;
-const PostAddNew = () => {
-  return <PostAddNewStyles></PostAddNewStyles>;
-};
-
-export default PostAddNew;
